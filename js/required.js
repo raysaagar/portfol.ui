093a4024f2756d7da94423638304415668e68c9b
Saagar Deshpande <raysaagar@gmail.com>
2012-12-29 01:43:55 -0500

    fixed markdown loader with tony's generic switch

--- js/required.js
+++ js/required.js
@@ -1,11 +1,36 @@
 var converter = new Showdown.converter();
 
-function load_markdown(mdfile, div){
-    $.get(mdfile, function(data) {
-		convert_markdown(data, div);
+// function load_markdown(mdfile, div){
+    // alert("hello");
+    // $.get(mdfile, function(data) {
+		// convert_markdown(data, div);
+    // });
+// };
+
+
+/* currently needs input as follows:
+    map
+        |--> data
+            |--> mdfile (this is the markdown file we want)
+            |--> div    (this is the div we want to fill with the converted markdown)
+    
+    currently div should be a string with # or . in front. 
+    TODO: make generic
+*/
+
+function load_markdown(map){
+    
+    $.get(map.data.mdfile, function(data) {
+        alert(data);
+		convert_markdown(data, map.data.div);
     });
 };
 
-function convert_markdown(mdcontents) {
-    div.html(converter.makeHtml(mdcontents));
+function convert_markdown(mdcontents,div) {
+    $(div).html(converter.makeHtml(mdcontents));
 }
+
+$(document).ready(function(){
+    $('#loadbtn').on("click",{mdfile: "markdown/test.md", div: "#output"}, load_markdown);
+    //$('#convertbtn').on("click",convert_markdown);
+});
---
+++ markdown/test.md
@@ -0,0 +1,13 @@
+# Parallel Sequitur #
+
+## Computer Science 205 Fall 2012
+
+### Project Members: ###
+* Saagar Deshpande (sdeshpande@college, saagar@hcs.harvard.edu)
+* Irene Chen (iychen@college)
+
+### Introduction: ###
+
+We implemented serial and parallel versions of the Sequitur compression algorithm. The Sequitur algorithm uses hierarchical structure and sequences of discrete symbols to compress files by exploiting repetative structures found in strings.
+
+Our final project is based on the algorithm as described in the following paper: "Identifying Hierarchical Structure in Sequences: A linear-time algorithm", Craig G. Nevill-Manning, Ian H. Witten, Journal of Artificial Intelligence Research 7 (1997) 67.82
